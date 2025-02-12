630114c8aac76f83d6a928dccd1a2e20d28013d8
yusuf <arasy541@gmail.com>
2025-02-12 15:28:45 +0300

    domainApplication
     domain and application udated entities added

---
+++ DugunSalonuKiralama/Core/DugunSalonuKiralama.Application/Interfaces/IWeddingRepository.cs
@@ -0,0 +1,17 @@
+﻿using DugunSalonuKiralama.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DugunSalonuKiralama.Application.Interfaces
+{
+    public interface IWeddingRepository
+    {
+        Task<List<Wedding>> GetCarsListWithBrands();
+        List<Wedding> GetLast5CarsWithBrands();
+        int GetCarCount();
+    }
+}
