293e80d64c50b2b0f912c8ed41c24af30aca48ec
yusuf <arasy541@gmail.com>
2025-02-27 11:12:28 +0300

    .
    .

--- DugunSalonuKiralama/Core/DugunSalonuKiralama.Application/Features/CQRS/Results/Booking/GetBookingQueryResult.cs
+++ DugunSalonuKiralama/Core/DugunSalonuKiralama.Application/Features/CQRS/Results/Booking/GetBookingQueryResult.cs
@@ -9,13 +9,5 @@ namespace DugunSalonuKiralama.Application.Features.CQRS.Results.Booking
 {
     public class GetBookingQueryResult
     {
-        public int Id { get; set; }
-        public int WeddingId { get; set; }
-        public WeddingHall WeddingHall { get; set; }
-        public int UserId { get; set; }
-        public DateTime BookingDate { get; set; }
-        public int GuestCount { get; set; }
-        public decimal TotalPrice { get; set; }
-        public string Status { get; set; }
     }
 }
