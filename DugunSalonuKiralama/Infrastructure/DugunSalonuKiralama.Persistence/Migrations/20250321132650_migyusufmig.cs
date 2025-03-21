c050c88953a03a11a22e7f0aebc10b35b0d752c5
yusuf <arasy541@gmail.com>
2025-03-21 16:31:58 +0300

    .
    .

---
+++ DugunSalonuKiralama/Infrastructure/DugunSalonuKiralama.Persistence/Migrations/20250321132650_migyusufmig.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DugunSalonuKiralama.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class migyusufmig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
